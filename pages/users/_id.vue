1e4c9307e0e6580657370e4ecc94f12b30c72b56
Matyukhin <AlexMtkhn@yandex.tu>
2022-09-06 18:13:41 +0300

    valivation in dinamic routing

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -3,3 +3,12 @@
     <h1>User {{ $route.params.id }}</h1>
   </section>
 </template>
+
+<script>
+export default {
+  validate({ params }) {
+    return /^\d+$/.test(params.id);
+  },
+};
+</script>
+
