60050cce6e724219eec9589c2e210e453866923d
Matyukhin <AlexMtkhn@yandex.tu>
2022-09-07 16:21:51 +0300

    loading of page

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,40 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-  mode: 'universal',
+  target: "static",
+  mode: "universal",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'app1',
+    title: "app1",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
+  loading: { color: "blue", height: "20px" },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/node_modules/bootstrap/dist/css/bootstrap.min.css'
-  ],
+  css: ["@/node_modules/bootstrap/dist/css/bootstrap.min.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/axios'
-  ],
+  modules: ["@nuxtjs/axios"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
