b9b1a0e155502ee3060a6777058426602fb45738
Matyukhin <AlexMtkhn@yandex.tu>
2022-09-07 15:34:47 +0300

    working with store

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -10,25 +10,24 @@
 </template>
 <script>
 export default {
-  async asyncData({ $axios }) {
-    const users = await $axios.$get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    return { users };
+  async fetch({ store }) {
+    if (store.getters["users/users"].length === 0) {
+      await store.dispatch("users/fetch");
+    }
   },
   data: () => ({
     pageTitle: "Users page",
   }),
-  async mounted() {
-    this.users = await this.$axios.$get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
+  computed: {
+    users() {
+      return this.$store.getters["users/users"];
+    },
   },
   methods: {
     openUser(user) {
       this.$router.push(`/users/${user.id}`);
     },
-  },
+  }
 };
 </script>
 
